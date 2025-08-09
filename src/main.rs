c384e5a4314d58d3ea99c5a1d58605c1afa77d4d
Tim <ttsutskarev@gmail.com>
2025-08-09 15:42:25 +0300

    Add parser for http response headers

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 mod network;
-mod utils;
+mod parser;
 mod types;
+mod utils;
 
 use crate::network::Request;
 
 fn main() {
     let url = network::URL::new("http://example.com");
     match url.get() {
-        Ok(resp) => println!("Response:\n{}", resp),
+        Ok(resp) => {
+            println!("Response:\n{}", resp);
+            let headers = parser::parse_response_headers(resp);
+            println!("{:#?}", headers);
+
+        }
         Err(e) => eprintln!("Error performing GET: {}", e),
     }
 }
---
+++ src/parser/mod.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+pub struct HTMLElement {
+    name: String,
+}
+
+pub fn parse_html(response: String) {
+    let chars = response.chars();
+    // Firstly we parse the headers into a data struct: dict?
+    // Secondly we parse the html into a custom data struct
+}
+
+pub fn parse_response_headers(response: String) -> HashMap<String, String> {
+    let mut headers = HashMap::<String, String>::new();
+    let mut buf = String::new();
+
+    let chars = response.chars();
+
+    for char in chars {
+        if char == '\n' {
+            let header: Vec<&str> = buf.trim().split(':').collect();
+            if header.len() < 2 {
+                buf.clear();
+            } else {
+                let h_key = header[0];
+                let h_val = header[1];
+                headers.insert(h_key.into(), h_val.trim().into());
+                buf.clear();
+            }
+            continue;
+        }
+        if char == '<' {
+            return headers
+        }
+        buf.push(char)
+    }
+    headers
+}
